c2d9c4a2319c852d836ca4432ebbc59667f418e9
Luis Restrepo <luchoposada555@gmail.com>
2023-09-26 22:56:20 -0500

    Ajustes de vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Luis-Restrepo98/my-books-app"
+  base: "https://Luis-Restrepo98/github.com/my-books-app",
 })
