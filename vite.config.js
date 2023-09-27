855c0df7448e5a4aeae42f00ec37bb199a94dcf9
Luis Restrepo <luchoposada555@gmail.com>
2023-09-26 22:42:11 -0500

    Ajustando rutas para deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Luis-Restrepo98"
+  base: "https://github.com/Luis-Restrepo98/my-books-app"
 })
