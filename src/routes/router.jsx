9bf4d7e208a8d194ac96ff981fae5ded8bdbc6b9
Luis Restrepo <luchoposada555@gmail.com>
2023-09-26 23:06:23 -0500

    complementacion de pages

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../pages/home/home'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../pages/home/Home';
 
 const Router = () => {
   return (
